6dfdc7af4eb2805f1a53ebf03adebbfa152fe0d1
Molly Seeley <seeley@mollys-air.home>
2020-09-28 18:33:36 -0400

    format CircularNav

--- src/components/CircularNav.js
+++ src/components/CircularNav.js
@@ -1,27 +1,15 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { Link } from "gatsby"
 import CircleType from "circletype"
 import "../styles/style.scss"
 
 const CircularNav = () => (
-  <nav>
-    <ul class="caps w-reg">
-      <li>
-        <Link to="#about">about</Link>
-      </li>
-      <li>
-        <Link to="#articles">articles</Link>
-      </li>
-      <li>
-        <Link to="#projects">projects</Link>
-      </li>
-      <li>
-        <Link to="/resume">resume</Link>
-      </li>
-      <li>
-        <Link to="#contact">contact</Link>
-      </li>
-    </ul>
+  <nav className="caps w-reg">
+    <Link to="#about">about</Link>
+    <Link to="#articles">articles</Link>
+    <Link to="#projects">projects</Link>
+    <Link to="/resume">resume</Link>
+    <Link to="#contact">contact</Link>
   </nav>
 )
 
